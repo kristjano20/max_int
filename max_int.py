5aecb630faaafed5e07feebe40840dd63536aaf6
kristjano <kristjano20@ru.is>
2021-09-02 14:26:43 +0000

    Algorithm written for max_int

---
+++ max_int.py
@@ -0,0 +1,10 @@
+#1 Ég tek inn tölu.
+#2 Ef hún er jákvæð þá skrái ég hana sem hæstu tölu
+#3 Ef hún er neikvæð þá hætti ég
+#4 Ég bið um aðra tölu.
+#5 Ef hún er jákvæð þá ber ég hana við hæstu töluna  mína
+#6 Ef nýja talan er stærri þá geri ég hana að hæstu tölunni
+#7 Ef hún er minni eða jafnstór þá geri ég ekkert
+#8 Ef nýja talan er neikvæð þá skila ég hæstu tölunni
+#9 Ég endurtek skref 5,6,7 þangað til að ég ég fæ neikvæða tölu
+
