9c604beaf52f4c84f98230c6320405460e4d7fac
kristjano <kristjano20@ru.is>
2021-09-02 15:11:24 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,10 @@
+#1 receive a number n that must be larger than zero 
+#2 write down 1,2,3 as the first three numbers of the sequence 
+#3 if n is 1, 2 or 3 then output 1; 1,2; and 1,2,3; respectfully and end. 
+#4 if n is larger than 3 then i start counting from 3 
+#5 add the sum of the first three numbers as the next number in the sequence 
+#6 add the sum of the last three numbers in the sequence and add the sum to
+#  the sequence
+#7 add one to the count for every sum you add to the sequence
+#8 when the count is equal to n then output the whole sequence and end
+#9 repeat steps 6,7,8
